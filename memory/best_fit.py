322ed16497535f679dd5defdfabec43e93a38b8b
Andrew417 <andrewehab417@gmail.com>
2026-05-06 12:52:09 +0300

    finished 2 Algos but not tested

--- memory/best_fit.py
+++ memory/best_fit.py
@@ -1,1 +1,19 @@
-  tables.py first_fit.
+def best_fit(process, holes_table, allocated_table):
+    holes_table.sort_holes()
+    for segment in process.segments:
+        best_hole = None
+        for hole in holes_table.holes:
+            if hole.size >= segment.size:
+                if best_hole is None or hole.size < best_hole.size:
+                    best_hole = hole
+
+        if best_hole is not None:
+            segment.base = best_hole.start_address
+
+            best_hole.size -= segment.size
+            best_hole.start_address += segment.size
+
+            if best_hole.size == 0:
+                holes_table.remove_hole(best_hole)
+
+            allocated_table.add_segment(segment)
--- memory/first_fit.py
+++ memory/first_fit.py
@@ -1,1 +1,17 @@
- 
+def first_fit(process, holes_table, allocated_table):
+    holes_table.sort_holes()
+    for segment in process.segments:
+        for hole in holes_table.holes:
+            if hole.size >= segment.size:
+                segment.base = hole.start_address
+
+                hole.size -= segment.size
+                hole.start_address += segment.size
+
+                if hole.size == 0:
+                    holes_table.remove_hole(hole)
+                
+
+                allocated_table.add_segment(segment)
+
+                break
